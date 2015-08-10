2175c80a538ec457a3b52f1b6e728761d37e873b
Nikolay <devmikola@gmail.com>
2015-08-10 15:45:45 +0300

    End of 6 chapter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   
 end
