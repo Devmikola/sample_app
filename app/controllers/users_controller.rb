efc0393d43ebbb9a4abb620ce6e934c426f50c5e
Nikolay <devmikola@gmail.com>
2015-08-25 18:41:12 +0300

    Chapter 11 Finished !

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :destroy, :edit, :update]
+  before_action :signed_in_user, only: [:index, :destroy, :edit, :update, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :facecontroll, only: [:new, :create]
@@ -54,7 +54,19 @@ class UsersController < ApplicationController
     end
   end
 
-  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end  
 
   private 
 
